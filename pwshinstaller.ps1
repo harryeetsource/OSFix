1715c1fd2d7b68705168b986e857c3a029a80b04
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2022-05-04 15:59:43 -0700

    Add files via upload

--- pwshinstaller.ps1
+++ pwshinstaller.ps1
@@ -1,5 +1,16 @@
 $folder1 = "$env:ProgramFiles\Powershell"
 $url1 = "https://github.com/PowerShell/PowerShell/releases/download/v7.2.3/PowerShell-7.2.3-win-x64.msi"
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process pwsh.exe -Verb RunAs -ArgumentList ('-noprofile -executionpolicy bypass -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
 if (Test-Path -Path $folder1) { Write-Host "powershell directory already exists, skipping" }
 
 else {
