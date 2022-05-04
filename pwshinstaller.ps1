18022686de00252002ede8931501b4e5c872f063
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2022-05-04 16:29:36 -0700

    Add files via upload

--- pwshinstaller.ps1
+++ pwshinstaller.ps1
@@ -1,5 +1,7 @@
 $folder1 = "$env:ProgramFiles\Powershell"
 $url1 = "https://github.com/PowerShell/PowerShell/releases/download/v7.2.3/PowerShell-7.2.3-win-x64.msi"
+$folder2 = "$env:ProgramFiles\clamav"
+$url2 = ""
 function Test-Admin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
     $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
@@ -16,4 +18,12 @@ else {
 New-Item -Path "$env:ProgramFiles\" -Name "powershell" -ItemType "directory"
 Invoke-WebRequest  $url1 -OutFile "$folder1\powershell.msi"
 Start-Process -Path "$folder1\powershell.msi"
+}
+if (Test-Path -Path $folder2) { Write-Host "clamav directory already exists, skipping" }
+
+else {
+New-Item -Path "$env:ProgramFiles\" -Name "clamav" -ItemType "directory"
+Invoke-WebRequest  $url1 -OutFile "$folder2\clamav\clamav.zip"
+Expand-Archive -LiteralPath "$folder2\clamav\clamav.zip" -DestinationPath "$folder2\"
+
 }
