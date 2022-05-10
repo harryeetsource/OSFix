15b6f34e278edc4e0daead1c6847340939e83d12
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2022-05-10 13:52:06 -0700

    Update osfix.ps1

--- osfix.ps1
+++ osfix.ps1
@@ -13,7 +13,7 @@ if ((Test-Admin) -eq $false)  {
     if ($elevated) {
         # tried to elevate, did not work, aborting
     } else {
-        Start-Process pwsh.exe -Verb RunAs -ArgumentList ('-noprofile -executionpolicy bypass -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -executionpolicy bypass -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     }
     exit
 }
