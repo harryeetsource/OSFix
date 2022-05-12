fc9b35dd8b5113d215bb23b4fbe900750eff35cc
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2022-05-12 09:29:08 -0700

    Add files via upload

---
+++ NetFx3Fix.ps1
@@ -0,0 +1,3 @@
+dism /online /enable-feature /featurename:netfx3 /all
+dism /online /enable-feature /featurename:WCF-HTTP-Activation /all
+dism /online /enable-feature /featurename:WCF-NonHTTP-Activation
